4f55409ebd92a83313db97225b49e340c200b7ba
male-gal <male-gal@student.42.fr>
2018-06-21 02:20:29 +0200

    adding activated to db

--- camagru/config/database.php
+++ camagru/config/database.php
@@ -1,7 +1,7 @@
 <?php
-$DB_DSN = 'mysql:host=localhost';
-$DB_USER = 'root';
-$DB_PASSWORD = '424242';
-$db = 'tried';
-$admin_pw = 'trolol';
+$DB_DSN = '';
+$DB_USER = '';
+$DB_PASSWORD = '';
+$db = '';
+$admin_pw = '';
 ?>
