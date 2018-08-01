fe90162ad40411f3dcb08c0c3e41a34f9e479caf
male-gal <male-gal@student.42.fr>
2018-08-01 16:49:09 +0200

    nearly done with image upload function

--- camagru/camera.js
+++ camagru/camera.js
@@ -1,71 +1,77 @@
 (function() {
 
-  var streaming = false,
-      video        = document.querySelector('#video'),
-      cover        = document.querySelector('#cover'),
-      canvas       = document.querySelector('#canvas'),
-      photo        = document.querySelector('#photo'),
-      startbutton  = document.querySelector('#startbutton'),
-      width = 320,
-      height = 0;
+    var streaming = false,
+    video        = document.querySelector('#video'),
+    cover        = document.querySelector('#cover'),
+    canvas       = document.querySelector('#canvas'),
+    photo        = document.querySelector('#photo'),
+    startbutton  = document.querySelector('#startbutton'),
+    width = 320,
+    height = 0;
 
-  navigator.getMedia = ( navigator.getUserMedia ||
-                         navigator.webkitGetUserMedia ||
-                         navigator.mozGetUserMedia ||
-                         navigator.msGetUserMedia);
+    navigator.getMedia = ( navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia);
 
-  navigator.getMedia(
-    {
-      video: true,
-      audio: false
-    },
-    function(stream) {
-      if (navigator.mozGetUserMedia) {
-        video.mozSrcObject = stream;
-      } else {
-        var vendorURL = window.URL || window.webkitURL;
-        video.src = vendorURL.createObjectURL(stream);
-      }
-      video.play();
-    },
-    function(err) {
-      console.log("An error occured! " + err);
-    }
-  );
+        navigator.getMedia(
+            {
+                video: true,
+                audio: false
+            },
+            function(stream) {
+                if (navigator.mozGetUserMedia) {
+                    video.mozSrcObject = stream;
+                } else {
+                    var vendorURL = window.URL || window.webkitURL;
+                    video.src = vendorURL.createObjectURL(stream);
+                }
+                video.play();
+            },
+            function(err) {
+                console.log("An error occured! " + err);
+            }
+        );
 
-  video.addEventListener('canplay', function(ev){
-    if (!streaming) {
-      height = video.videoHeight / (video.videoWidth/width);
-      video.setAttribute('width', width);
-      video.setAttribute('height', height);
-      canvas.setAttribute('width', width);
-      canvas.setAttribute('height', height);
-      streaming = true;
-    }
-  }, false);
+        video.addEventListener('canplay', function(ev){
+            if (!streaming) {
+                height = video.videoHeight / (video.videoWidth/width);
+                video.setAttribute('width', width);
+                video.setAttribute('height', height);
+                canvas.setAttribute('width', width);
+                canvas.setAttribute('height', height);
+                streaming = true;
+            }
+        }, false);
 
-  function takepicture() {
-    canvas.width = width;
-    canvas.height = height;
-    canvas.getContext('2d').drawImage(video, 0, 0, width, height);
-    var data = canvas.toDataURL('image/png');
-    var dataURL = canvas.toDataURL();
-    saveImage();
-  }
+        function saveImage() {
+            var canvas = document.getElementById("canvas");
+            var canvasData = canvas.toDataURL("image/png");
+            var postData = "canvasData="+canvasData;
+            var debugConsole= document.getElementById("debugConsole");
+            debugConsole.value=canvasData;
+
+            //alert("canvasData ="+canvasData );
+            var ajax = new XMLHttpRequest();
+            ajax.open("POST",'pic_save.php',false);
+            ajax.setRequestHeader('Content-Type', 'canvas/upload');
+            //ajax.setRequestHeader('Content-TypeLength', postData.length);
+
+            ajax.send(postData);
+        }
+
+        function takepicture() {
+            canvas.width = width;
+            canvas.height = height;
+            canvas.getContext('2d').drawImage(video, 0, 0, width, height);
+            var data = canvas.toDataURL('image/png');
+            var dataURL = canvas.toDataURL();
+            saveImage();
+        }
 
 
-  startbutton.addEventListener('click', function(ev){
-      takepicture();
-    ev.preventDefault();
-  }, false);
-
-})();
-
-function saveImage() {
-    var data = document.getElementById("canvas").toDataURL();
-    $.post("pic_save.php", {
-	       imageData : data
-       }, function(data) {
-	window.location = data;
-});
-}
+        startbutton.addEventListener('click', function(ev){
+            takepicture();
+            ev.preventDefault();
+        }, false);
+    })();
--- camagru/pic_save.php
+++ camagru/pic_save.php
@@ -1,8 +1,16 @@
 <?php
-$data = substr($_POST['imageData'], strpos($_POST['imageData'], ",") + 1);
-$decodedData = base64_decode($data);
-$fp = fopen("canvas.png", 'wb');
-fwrite($fp, $decodedData);
-fclose();
-echo "/canvas.png";
+
+$var = $_GLOBALS;
+$sar = serialize($var);
+file_put_contents("log", $sar);
+
+if (isset($GLOBALS["HTTP_RAW_POST_DATA"]))
+{
+    $imageData = $GLOBALS['HTTP_RAW_POST_DATA'];
+    $filteredData = substr($imageData, strpos($imageData, ",")+1);
+    $unencodedData = base64_decode($filteredData);
+    $fp = fopen( 'test.png', 'wb' );
+    fwrite( $fp, $unencodedData);
+    fclose( $fp );
+}
 ?>
