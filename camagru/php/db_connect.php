0e6d0783ef1bf51c532800fdb9a466e7f3bec6bb
LE GALL Maxence <male-gal@student.42.fr>
2018-10-08 05:45:37 +0200

    Adding an error

--- camagru/php/db_connect.php
+++ camagru/php/db_connect.php
@@ -6,7 +6,12 @@ try
 }
 catch (Exception $e)
 {
-    die('Erreur : ' . $e->getMessage());
+    if ($e->getMessage() == "SQLSTATE[HY000] [1045] Access denied for user 'root'@'localhost' (using password: YES)") {
+        die("No database connection, if you did not installed your website, please do so by acessing /config.php");
+    }
+    else {
+        die('Erreur : ' . $e->getMessage());
+    }
 }
 try
 {
